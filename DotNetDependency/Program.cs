356c20fee269cae215a8fbdffb2eec41b9b381f0
Rakhi Vijay Kumar <rakhiv.kumar@ltts.com>
2021-05-03 09:20:02 +0530

    Add project files.

---
+++ DotNetDependency/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Humanizer;
+namespace DotNetDependency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("car".Pluralize());
+            Console.WriteLine("octopus".Pluralize());
+            Console.WriteLine(1003.ToWords());
+        }
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1() => Assert.True(true);
+        [Fact]
+        public void Test2()
+        {
+
+        }
+    }
+}
